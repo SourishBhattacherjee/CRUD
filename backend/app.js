85a23186b9350d98c8d54f3dae47990d6b9e84aa
SourishBhattacherjee <sourishbhatt3302@gmail.com>
2026-02-21 23:43:10 +0530

    ,

--- backend/app.js
+++ backend/app.js
@@ -13,12 +13,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/tasks", taskRoutes);
 
 app.get("/", (req, res) => res.send("API running"));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
